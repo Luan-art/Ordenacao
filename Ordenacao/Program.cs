b5c35c4e61a8c4e374cea0243ff57a52ae53e643
Luan-art <luanlf2003@gmail.com>
2024-04-23 18:43:04 -0300

    retiradaReptidos

--- Ordenacao/Program.cs
+++ Ordenacao/Program.cs
@@ -1,22 +1,25 @@
 ﻿using System.Numerics;
 using static System.Net.Mime.MediaTypeNames;
 
-int aux, max = 7;
+int aux, max = 5;
 int[] vetores = new int[max];
 int[] vetorCopia = new int[max];
-
+int[] vetorInverso = new int[max];
 
 for(int i = 0; i < max; i++)
 {
-    vetores[i] = new Random().Next(-100, 100);
+    vetores[i] = new Random().Next(0, 5);
     vetorCopia[i] = vetores[i];
-
 }
-    
+
+for (int i = 0; i < max; i++)
+    Console.WriteLine("O Original:  " + vetores[i]);
+
+Console.WriteLine();
+
 for (int i = 0; i < max; i++)
 {
-    
-    for(int j = i + 1; j < max; j++)
+    for (int j = i + 1; j < max; j++)
     {
         if (vetorCopia[j] < vetorCopia[i])
         {
@@ -24,17 +27,53 @@ for (int i = 0; i < max; i++)
             vetorCopia[i] = vetorCopia[j];
             vetorCopia[j] = aux;
 
-           
         }
     }
-
-    
 }
 
 
+
+for (int i = 1; i < max; i++)
+{
+    if (vetorCopia[i] == vetorCopia[i - 1])
+    {
+        for (int j = i; j < max - 1; j++)
+        {
+            vetorCopia[j] = vetorCopia[j + 1];
+        }
+        max--;
+        i--;
+    }
+}
+
 for (int i = 0; i < max; i++)
-    Console.WriteLine("O menor é:  " + vetorCopia[i]);
-    
+    Console.WriteLine("Do menor é:  " + vetorCopia[i]);
+
+for (int i = 0; i < max; i++)
+{
+    vetorInverso[i] = vetorCopia[i];
+}
+
+for (int i = 0; i < max; i++)
+{
+    for (int j = i + 1; j < max; j++)
+    {
+        if (vetorInverso[j] > vetorInverso[i])
+        {
+            aux = vetorInverso[i];
+            vetorInverso[i] = vetorInverso[j];
+            vetorInverso[j] = aux;
+
+        }
+    }
+}
+
+Console.WriteLine("\n");
+
+for (int i = 0; i < max; i++)
+    Console.WriteLine("Do maior é:  " + vetorInverso[i]);
+
+
 
 
 
